94ebc2fea92779b67c681a7a35aaeaddb29c4392
hedong zhao <zhaohedong@gmail.com>
2019-03-15 11:05:22 +0800

    ddy

--- test_projects/testdir/py/c.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-  
-#!/usr/bin/env python  
-
-import urllib
-my_text = '你想搜索的东西'
-url = 'http://www.baidu.com/s?wd=%s' % urllib.requests.quote(my_text)
-result = urllib.requests.urlopen(url).read()  # 搜索结果页的内容
-print(result)  
