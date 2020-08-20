050a6815b595ea12388223e70e191e4a7db15191
zhaohedong <zhao-hd@neusoft.com>
2020-08-20 19:23:14 +0800

    log.c pointer.c

--- TestProjects/c/pointer.c
+++ TestProjects/c/pointer.c
@@ -14,29 +14,47 @@ typedef struct {
     B* bStu;
 } A;
 
-void CopyStuA(A *p, A **pp)   
-{   
+void CopyStuA(A** pp)   
+{   A from;
+    from.a = 1;
+    from.b = 2;
+    from.bStu = (B*)malloc(sizeof(B));
+    from.bStu->c = 3;
+    from.bStu->d = 4; 
+    A* p = &from;
+
     *pp = (A*)malloc(sizeof(A));
     (*pp)->a = p->a;
     (*pp)->b = p->b;
     (*pp)->bStu = (B*)malloc(sizeof(B));
     (*pp)->bStu->c = p->bStu->c;
     (*pp)->bStu->d = p->bStu->d;
+    free(from.bStu);
+}
+
+void CopyWapper2(A** pp)
+{
+    CopyStuA(pp);
+}
+
+void CopyWapper1(A** pp)
+{
+    CopyWapper2(pp);
+}
+
+void CopyWapper(A** pp)
+{
+    CopyWapper1(pp);
 }
 
 void Test (void)   
 {   
-    A from;
-    from.a = 1;
-    from.b = 2;
-    from.bStu = (B*)malloc(sizeof(B));
-    from.bStu->c = 3;
-    from.bStu->d = 4;
-
     A* to = NULL;
-    CopyStuA(&from, &to);
+    CopyWapper(&to);
     printf("after copy to.a = %d, to.b = %d, to.bStu->c = %d, to.bStu->d = %d\n",
             to->a, to->b, to->bStu->c, to->bStu->d);
+    free(to->bStu);
+    free(to);
 }
 
 int main()  
